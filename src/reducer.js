b141ba2160daa16828d14f205d360de963e05d65
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-04 21:34:05 +0500

    remove single item from cart functionality added

--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,13 @@ const reducer = (state, action) => {
     return { ...state, cart: [] };
     // return { loading: false, cart: [], total: 0, amount: 0 };
   }
+  if (action.type === 'REMOVE_ITEM') {
+    console.log('remove item running');
+    return {
+      ...state,
+      cart: state.cart.filter((item) => item.id !== action.payload),
+    };
+  }
   return state;
 };
 
